a42c210061ad40bcfadab4e3b06b19fda280a7e8
TakuroSemba <171462421+BASEN69@users.noreply.github.com>
2024-07-10 09:41:19 +0000

    少し追加　7/10

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  
+  #画像エラー回避
+  config.active_job.queue_adapter = :inline
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+  get 'users/index'
+  get 'users/show'
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
   root to: 'homes#top'
   devise_for :users
   get 'homes/about' => 'homes#about', as: 'about'
