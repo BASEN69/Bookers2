f059d7fd9d6b081f486527c88fe12f37d17180df
TakuroSemba <171462421+BASEN69@users.noreply.github.com>
2024-07-11 09:46:15 +0000

    bookers2  ユーザー編集　投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-  get 'users/index'
-  get 'users/show'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
+
   root to: 'homes#top'
   devise_for :users
+
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+
+  resources :users, only: [:show, :edit, :update, :index]
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
