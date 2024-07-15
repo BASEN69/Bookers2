98d8f81388427da7b49922cfda5e6bc3dab1b0a4
TakuroSemba <171462421+BASEN69@users.noreply.github.com>
2024-07-15 10:18:52 +0000

    リンクおかしくなった

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,36 @@
 class UsersController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
 
   def show
     @user = User.find(params[:id])
-    
+    @book = Book.new
   end
-  
+
   def index
     @book = Book.new
     @book.user_id = current_user.id
-    @books = Book.all
+
     @users = User.all
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path
   end
-  
-  
+
+
   private
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
   #ユーザー本人のみアクセス可能にする
   def is_matching_login_user
     user = User.find(params[:id])
